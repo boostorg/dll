a6329e2738d2d415dc4feb7d19211bab9232fa81
Fedor Osetrov <fiosetrova@gmail.com>
2026-04-19 07:57:52 +0000

    add copyright

--- include/boost/dll/detail/config.hpp
+++ include/boost/dll/detail/config.hpp
@@ -1,3 +1,9 @@
+// Copyright Fedor Osetrov, 2025-2026.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef BOOST_DLL_DETAIL_CONFIG_HPP
 #define BOOST_DLL_DETAIL_CONFIG_HPP
 
